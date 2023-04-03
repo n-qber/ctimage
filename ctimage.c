5472ce0bdd16858a4d18eb6771a1be1a12de35c3
Nicolas <nmkbertozzo@gmail.com>
2023-04-03 12:22:46 -0300

    Resizing images to fit the current terminal

--- ctimage.c
+++ ctimage.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ncurses.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb/stb_image_resize.h"
 
 void show_help_message(void);
 
@@ -35,12 +38,22 @@ void show_image(char imagepath[255], char separator[15]){
   int width, height, channels;
   unsigned char *img = stbi_load(imagepath, &width, &height, &channels, 0);
 
-  for(int line=0; line<height;line += 2){
-    for(int chr=0; chr<width; chr += 2){
+  int new_width, new_height;
+
+  initscr();
+  getmaxyx(stdscr, new_height, new_width);
+  endwin();
+
+  unsigned char *resized_image = (unsigned char*)malloc(new_width * new_height * channels);
+
+  stbir_resize_uint8(img, width, height, 0, resized_image, new_width, new_height, 0, channels);
+
+  for(int line=0; line<new_height;line += 2){
+    for(int chr=0; chr<new_width; chr += 2){
 
       printf("\x1b[48;2");
       for(int chan=0; chan<channels;chan++){
-        printf(";%d", img[line*width*channels + chr*channels + chan]);
+        printf(";%d", resized_image[line*new_width*channels + chr*channels + chan]);
       }
       printf("m");
       printf(separator);
@@ -50,6 +63,7 @@ void show_image(char imagepath[255], char separator[15]){
   }
 
   stbi_image_free(img);
+  free(resized_image);
 }
 
 void show_help_message(void){
